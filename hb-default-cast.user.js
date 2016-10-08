e32a05068b8e933ea245c54f727d992c72255433
synthtech <synthtech0@gmail.com>
2016-10-07 20:06:20 -0400

    Update jquery

--- hb-default-cast.user.js
+++ hb-default-cast.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         HB Default Japanese Cast
 // @description  Default to Japanese cast on anime pages
-// @version      1.0
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js
+// @version      1.0.1
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js
 // @require      https://cdn.rawgit.com/synthtech/d2627fbad7e51c96c642/raw/e539a6f16c10465eb948b9ef6b0fe1d4c17a7c3e/jquery.waituntilexists.min.js
 // @match        *://hummingbird.me/*
 // @grant        none
--- hb-franchise.user.js
+++ hb-franchise.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         HB Hide Empty Franchise
 // @description  Hide empty franchise panel on anime pages
-// @version      1.0.1
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js
+// @version      1.0.2
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js
 // @require      https://cdn.rawgit.com/synthtech/d2627fbad7e51c96c642/raw/e539a6f16c10465eb948b9ef6b0fe1d4c17a7c3e/jquery.waituntilexists.min.js
 // @include      *://hummingbird.me/*
 // @grant        none
