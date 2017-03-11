d5d89bc0ae2323411c29c8e4ac3cefe4c8bd375f
synthtech <synthtech0@gmail.com>
2017-03-10 19:03:47 -0500

    Rewrite loop script

--- kitsu-loop.user.js
+++ kitsu-loop.user.js
@@ -1,20 +1,17 @@
 // ==UserScript==
 // @name         Kitsu Video Looper
 // @description  Loop videos
-// @version      2.0
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js
-// @require      https://cdn.rawgit.com/synthtech/d2627fbad7e51c96c642/raw/e539a6f16c10465eb948b9ef6b0fe1d4c17a7c3e/jquery.waituntilexists.min.js
-// @include      *://kitsu.io/*
+// @version      2.0.1
+// @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
+// @match        *://kitsu.io/*
 // @grant        none
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true);
-var kitsuLoop = function() {
-    var loopVid = function() {
-        var vids = document.getElementsByTagName("video");
-        for (i = 0; i < vids.length; i++) vids[i].setAttribute("loop", "true");
-        void 0;
-    };
-    $(".stream-item .stream-content-post video").waitUntilExists(loopVid);
-    $(".stream-item .stream-item-comments .comment-body video").waitUntilExists(loopVid);
-}();
+(function() {
+    waitForElems({
+        sel: ".stream-item video",
+        onmatch: function(elem) {
+            elem.setAttribute("loop", true);
+        }
+    });
+})();
