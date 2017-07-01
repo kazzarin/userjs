29e3b2511564d948f4d067a9c1df5faf0612d7d2
synthtech <synthtech0@gmail.com>
2017-07-01 02:37:48 -0400

    Kitsu: Cleanup character script

--- kitsu-character-link.user.js
+++ kitsu-character-link.user.js
@@ -8,24 +8,18 @@
 // ==/UserScript==
 
 (function() {
-    var SCRIPT_NAME = 'Kitsu Character Link';
     var API = 'https://kitsu.io/api/edge';
 
-    var Util = {
-        log: function() {
-            var args = [].slice.call(arguments);
-            args.unshift('%c' + SCRIPT_NAME + ':', 'font-weight: bold;color: #233c7b;');
-            console.log.apply(console, args);
-        },
-        q: function(query, context) {
-            return (context || document).querySelector(query);
-        },
-        qq: function(query, context) {
-            return [].slice.call((context || document).querySelectorAll(query));
-        }
-    };
-
     var App = {
+        checkImage: function(elem, cb) {
+            if (elem.src) {
+                cb(elem.src.match(/images\/([0-9]+)\//)[1]);
+            } else if (elem.hasAttribute('data-src')) {
+                cb(elem.getAttribute('data-src').match(/images\/([0-9]+)\//)[1]);
+            } else {
+                cb(null);
+            }
+        },
         getMalId: function(id, cb) {
             GM_xmlhttpRequest({
                 method: 'GET',
@@ -38,11 +32,11 @@
                         var json = JSON.parse(response.responseText);
                         cb(json.data.attributes.malId);
                     } catch (err) {
-                        Util.log('Failed to parse character API results');
+                        console.log('Failed to parse character API results');
                     }
                 },
                 onerror: function() {
-                    Util.log('Failed to get Kitsu character data');
+                    console.log('Failed to get Kitsu character data');
                 }
             });
         }
@@ -53,17 +47,16 @@
         sel: '.favorite-characters-panel img',
         stop: false,
         onmatch: function(character) {
-            if (character.src) {
-                var id = character.src.match(/images\/([0-9]+)\//)[1];
-            } else if (character.hasAttribute('data-src')) {
-                var id = character.getAttribute('data-src').match(/images\/([0-9]+)\//)[1];
-            }
-            App.getMalId(id, function(malId) {
-                if (malId) {
-                    var link = character.parentElement.parentElement;
-                    link.href = 'https://myanimelist.net/character/' + malId;
-                    link.target = '_blank';
-                    link.rel = 'noopener noreferrer';
+            App.checkImage(character, function(id) {
+                if (id) {
+                    App.getMalId(id, function(malId) {
+                        if (malId) {
+                            var link = character.parentElement.parentElement;
+                            link.href = 'https://myanimelist.net/character/' + malId;
+                            link.target = '_blank';
+                            link.rel = 'noopener noreferrer';
+                        }
+                    });
                 }
             });
         }
@@ -74,18 +67,22 @@
         sel: '.waifu-wrapper img',
         stop: false,
         onmatch: function(waifu) {
-            var id = waifu.src.match(/images\/([0-9]+)\//)[1];
-            App.getMalId(id, function(malId) {
-                if (malId) {
-                    var name = Util.q('.waifu-name');
-                    var link = document.createElement('a');
-                    link.textContent = name.textContent;
-                    link.style = 'font-family:inherit';
-                    link.href = 'https://myanimelist.net/character/' + malId;
-                    link.target = '_blank';
-                    link.rel = 'noopener noreferrer';
-                    name.textContent = '';
-                    name.appendChild(link);
+            App.checkImage(waifu, function(id) {
+                if (id) {
+                    App.getMalId(id, function(malId) {
+                        if (malId) {
+                            //var name = waifu.parentNode.nextSibling;
+                            var name = document.querySelector('.waifu-name');
+                            var link = document.createElement('a');
+                            link.textContent = name.textContent;
+                            link.style.fontFamily = 'inherit';
+                            link.href = 'https://myanimelist.net/character/' + malId;
+                            link.target = '_blank';
+                            link.rel = 'noopener noreferrer';
+                            name.textContent = '';
+                            name.appendChild(link);
+                        }
+                    });
                 }
             });
         }
@@ -96,17 +93,16 @@
         sel: '.character-grid .character-image img',
         stop: false,
         onmatch: function (character) {
-            if (character.src) {
-                var id = character.src.match(/images\/([0-9]+)\//)[1];
-            } else if (character.hasAttribute('data-src')) {
-                var id = character.getAttribute('data-src').match(/images\/([0-9]+)\//)[1];
-            }
-            App.getMalId(id, function(malId) {
-                if (malId) {
-                    var link = character.parentElement.parentElement;
-                    link.href = 'https://myanimelist.net/character/' + malId;
-                    link.target = '_blank';
-                    link.rel = 'noopener noreferrer';
+            App.checkImage(character, function(id) {
+                if (id) {
+                    App.getMalId(id, function(malId) {
+                        if (malId) {
+                            var link = character.parentElement.parentElement;
+                            link.href = 'https://myanimelist.net/character/' + malId;
+                            link.target = '_blank';
+                            link.rel = 'noopener noreferrer';
+                        }
+                    });
                 }
             });
         }
