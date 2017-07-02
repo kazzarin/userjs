65de2b831c63a08213e1b2475e9acbe37127296f
synthtech <synthtech0@gmail.com>
2017-07-01 23:43:31 -0400

    Kitsu: Update admin script

--- kitsu-admin.user.js
+++ kitsu-admin.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Kitsu Admin
-// @description  Small changes to rails admin for convenience
-// @version      1.0
+// @namespace    https://github.com/synthtech
+// @description  Minor changes to rails admin for convenience
+// @version      1.1
+// @author       synthtech
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
 // @match        *://kitsu.io/api/admin/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(() => {
     // Show full history message
     waitForElems({
         sel: '#history tbody tr td:last-child',
-        stop: false,
-        onmatch: function(elem) {
-            elem.style.whiteSpace = 'normal';
+        onmatch({style}) {
+            style.whiteSpace = 'normal';
         }
     });
 
     // Open "show in app" link in new tab
     waitForElems({
         sel: '.show_in_app_member_link a',
-        stop: false,
-        onmatch: function(elem) {
+        onmatch(elem) {
             elem.target = '_blank';
         }
     });
