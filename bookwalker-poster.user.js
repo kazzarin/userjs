15b9d36a3a99293fb34d8905fab28a94c344d032
synthtech <synthtech0@gmail.com>
2017-07-02 02:59:27 -0400

    Bookwalker: Update script

--- bookwalker-poster.user.js
+++ bookwalker-poster.user.js
@@ -1,23 +1,24 @@
 // ==UserScript==
 // @name         Bookwalker Poster
+// @namespace    https://github.com/synthtech
 // @description  Create link to full-size poster images
-// @version      1.0
+// @version      1.1
+// @author       synthtech
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
 // @match        *://bookwalker.jp/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    var REGEX = /^https?:\/\/bookwalker\.jp\/de[a-z0-9\-]+\/$/;
+(() => {
+    const REGEX = /^https?:\/\/bookwalker\.jp\/de[a-z0-9\-]+\/$/;
 
-    // Create link
-    waitForUrl(REGEX, function() {
-        var poster = document.querySelector('meta[property="og:image"]').content;
+    waitForUrl(REGEX, () => {
+        let poster = document.querySelector('meta[property="og:image"]').content;
         waitForElems({
             sel: '.main-cover .main-larger',
             stop: true,
-            onmatch: function(elem) {
-                var link = document.createElement('a');
+            onmatch(elem) {
+                let link = document.createElement('a');
                 link.href = poster;
                 link.target = '_blank';
                 link.text = 'View Original Image';
