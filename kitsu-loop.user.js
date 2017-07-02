160e4e93e6c00000eb55aef04cc4ebd44f16057b
synthtech <synthtech0@gmail.com>
2017-07-01 23:35:03 -0400

    Kitsu: Update loop script

--- kitsu-loop.user.js
+++ kitsu-loop.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         Kitsu Video Looper
+// @namespace    https://github.com/synthtech
 // @description  Loop videos
-// @version      2.0.1
+// @version      2.0.2
+// @author       synthtech
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
 // @match        *://kitsu.io/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(() => {
     waitForElems({
-        sel: ".stream-item video",
-        onmatch: function(elem) {
-            elem.setAttribute("loop", true);
+        sel: '.stream-item video',
+        onmatch(elem) {
+            elem.loop = true;
         }
     });
 })();
