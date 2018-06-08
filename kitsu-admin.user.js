eaf0581472e26f8d6538799b41fec4df3f866245
synthtech <synthtech0@gmail.com>
2018-06-08 15:01:51 -0400

    Collapse all empty fields

--- kitsu-admin.user.js
+++ kitsu-admin.user.js
@@ -2,7 +2,7 @@
 // @name         Kitsu Admin
 // @namespace    https://github.com/synthtech
 // @description  Minor changes to rails admin for convenience
-// @version      1.2.0
+// @version      1.3.0
 // @author       synthtech
 // @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
 // @match        *://kitsu.io/api/admin/*
@@ -50,6 +50,73 @@
         onmatch(date) {
             date.textContent = moment.utc(date.textContent, format).local().format(format);
             date.title = date.textContent;
+        }
+    });
+
+    // Collapse fields
+    waitForElems({
+        sel: 'fieldset > div',
+        onmatch(elem) {
+            const label = elem.firstChild;
+            const field = elem.lastChild;
+            const id = `${elem.id}_collapse`;
+            field.classList.add('collapse');
+            field.id = id;
+            label.tagName = 'a';
+            label.setAttribute('data-toggle','collapse');
+            label.setAttribute('data-target',`#${id}`);
+
+            const check = field.parentElement.classList;
+            let checkType;
+            for (let i = 0; i < check.length; i++) {
+                if (check[i].includes('_type')) {
+                    checkType = check[i];
+                    break;
+                }
+            }
+
+            switch(checkType) {
+                case 'date_type':
+                    showField('input');
+                    break;
+                case 'enum_type':
+                    showField();
+                    break;
+                case 'has_and_belongs_to_many_association_type':
+                    showField('select');
+                    break;
+                case 'has_many_association_type':
+                    showField('select');
+                    break;
+                case 'integer_type':
+                    showField();
+                    break;
+                case 'paperclip_type':
+                    showField('.toggle :first-child');
+                    break;
+                case 'serialized_type':
+                    showField();
+                    break;
+                case 'string_type':
+                    showField();
+                    break;
+                case 'text_type':
+                    showField();
+                    break;
+            }
+
+            function showField(select) {
+                if (select) {
+                    let fieldInput = field.querySelector(select);
+                    if (fieldInput.value !== '') {
+                        $(`#${id}`).collapse('show');
+                    }
+                } else {
+                    if (field.firstChild.value !== '') {
+                        $(`#${id}`).collapse('show');
+                    }
+                }
+            }
         }
     });
 })();
