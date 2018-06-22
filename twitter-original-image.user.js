5b4e527d5ee72b12883b0f799181a6ab00db688d
synthtech <synthtech0@gmail.com>
2018-06-22 15:51:48 -0400

    Add Twitter Original Image script

---
+++ twitter-original-image.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Twitter Original Image
+// @namespace    https://github.com/synthtech
+// @description  Redirect to original images
+// @version      1.0
+// @author       synthtech
+// @match        *://pbs.twimg.com/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    const imageRegex = /^https?:\/\/pbs\.twimg\.com\/media\/([a-zA-Z0-9_]+)\.(jpg|jpeg|png|gif)((:[a-z]+)|(\?name=[a-z]+))?$/;
+    let url = location.href;
+
+    if (url.match(imageRegex)) {
+        if (url.match(imageRegex)[5]) {
+            if (url.match(imageRegex)[5] !== '?name=orig') {
+                redirect(url);
+            }
+        } else {
+            redirect(url);
+        }
+    }
+
+    function redirect(url) {
+        location.href = `https://pbs.twimg.com/media/${url.match(imageRegex)[1]}.${url.match(imageRegex)[2]}?name=orig`;
+    }
+})();
