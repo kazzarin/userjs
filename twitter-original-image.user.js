ac2436df4cbf34cf5bb6b2a388f455c5b0a41efc
synthtech <synthtech0@gmail.com>
2019-02-19 21:52:20 -0500

    Redirect new twitter url format

--- twitter-original-image.user.js
+++ twitter-original-image.user.js
@@ -2,27 +2,33 @@
 // @name         Twitter Original Image
 // @namespace    https://github.com/synthtech
 // @description  Redirect to original images
-// @version      1.0.1
+// @version      2.0.0
 // @author       synthtech
 // @match        *://pbs.twimg.com/*
 // @grant        none
 // ==/UserScript==
 
 (() => {
+    // .jpg:orig
+    // .jpg?name=orig
     const imageRegex = /^https?:\/\/pbs\.twimg\.com\/media\/([a-zA-Z0-9_\-]+)\.(jpg|jpeg|png|gif)((:[a-z]+)|(\?name=[a-z]+))?$/;
+    // ?format=jpg&name=4096x4096
+    const imageRegex2 = /^https?:\/\/pbs\.twimg\.com\/media\/([a-zA-Z0-9_\-]+)\?format=(jpg|jpeg|png|gif)&name=([0-9]+\x[0-9]+)?$/;
     let url = location.href;
 
     if (url.match(imageRegex)) {
         if (url.match(imageRegex)[5]) {
             if (url.match(imageRegex)[5] !== '?name=orig') {
-                redirect(url);
+                redirect(url.match(imageRegex)[1], url.match(imageRegex)[2]);
             }
         } else {
-            redirect(url);
+            redirect(url.match(imageRegex)[1], url.match(imageRegex)[2]);
         }
+    } else if (url.match(imageRegex2)) {
+        redirect(url.match(imageRegex2)[1], url.match(imageRegex2)[2]);
     }
 
-    function redirect(url) {
-        location.href = `https://pbs.twimg.com/media/${url.match(imageRegex)[1]}.${url.match(imageRegex)[2]}?name=orig`;
+    function redirect(id, format) {
+        location.href = `https://pbs.twimg.com/media/${id}.${format}?name=orig`;
     }
 })();
