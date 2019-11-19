6b8743115375c3dbf58426f5c5f4dfdaa5399465
synthtech <synthtech0@gmail.com>
2019-11-19 00:03:52 -0500

    Update Bookwalker script

--- bookwalker-poster.user.js
+++ bookwalker-poster.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Bookwalker Poster
 // @namespace    https://github.com/synthtech
-// @description  Create link to full-size poster images
-// @version      1.1.1
+// @description  Add links to cover images
+// @version      1.2
 // @author       synthtech
-// @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
-// @match        *://bookwalker.jp/*
+// @require      https://gitcdn.xyz/cdn/fuzetsu/userscripts/ab01548c6ebdead7781307d02e9882b1e8fe64dd/wait-for-elements/wait-for-elements.js
+// @match        *://*.bookwalker.jp/*
 // @grant        none
 // ==/UserScript==
 
 (() => {
-    const REGEX = /^https?:\/\/bookwalker\.jp\/de[a-z0-9\-]+\/$/;
+    const REGEX = /^https?:\/\/(r18\.)?bookwalker\.jp\/de[a-z0-9\-]+\/$/;
 
     waitForUrl(REGEX, () => {
-        let poster = document.querySelector('meta[property="og:image"]').content;
+        const poster = document.querySelector('.main-cover .main-cover-inner img').src;
         waitForElems({
             sel: '.main-cover .main-larger',
             stop: true,
             onmatch(elem) {
-                let link = document.createElement('a');
+                const link = document.createElement('a');
                 link.href = poster;
                 link.target = '_blank';
-                link.text = 'View Original Image';
+                link.text = 'View Cover Image';
                 link.className = 'main-larger-text';
                 elem.appendChild(link);
             }
