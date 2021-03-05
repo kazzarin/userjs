6bb033b7d321c8d2463cdafb0d57263323ac3c3b
synthtech <synthtech0@gmail.com>
2021-03-05 02:08:47 -0500

    Update anilist-mal.user.js

--- anilist-mal.user.js
+++ anilist-mal.user.js
@@ -2,7 +2,7 @@
 // @name         AniList MAL Links
 // @namespace    https://github.com/synthtech
 // @description  Add links to MAL on media pages
-// @version      2.1
+// @version      2.2
 // @author       synthtech
 // @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@ec863aa92cea78a20431f92e80ac0e93262136df/wait-for-elements/wait-for-elements.js
 // @match        *://anilist.co/*
@@ -88,6 +88,7 @@
         return elem;
     }
 
+    // TODO: Split this up and add more URL checks before checking storage
     async function getLink(elem) {
         const [, media, id] = location.href.match(REGEX);
         const malId = await checkStore(parseInt(id));
@@ -107,9 +108,11 @@
         }
     }
 
+    // TODO: Need to find a more reliable way to watch route changes from Vue router
     waitForUrl(REGEX, () => {
         waitForElems({
             sel: '.media .header .content > h1',
+            stop: true,
             onmatch: async (elem) => {
                 await getLink(elem);
             },
