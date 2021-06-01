3adb14040010e6c3558b8d2a4bef45ea19a86fc5
synthtech <synthtech0@gmail.com>
2021-05-31 20:49:41 -0400

    Update giphy-redirect.user.js

--- giphy-redirect.user.js
+++ giphy-redirect.user.js
@@ -2,7 +2,7 @@
 // @name         Giphy Redirect
 // @namespace    https://github.com/synthtech
 // @description  Redirect giphy pages to gif
-// @version      1.0.1
+// @version      1.1.0
 // @author       synthtech
 // @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
 // @match        *://giphy.com/*
@@ -17,8 +17,10 @@
             sel: 'video',
             stop: true,
             onmatch(elem) {
-                const gif = elem.poster.replace('/giphy_s.gif', '/giphy.gif');
-                location.replace(gif);
+                if (elem.poster) {
+                    const gif = elem.poster.replace('/giphy_s.gif', '/source.gif');
+                    location.replace(gif);
+                }
             },
         });
     });
