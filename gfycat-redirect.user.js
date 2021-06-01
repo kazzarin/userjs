b4c646fc40e3821eeb9145656e78a28acfa37963
synthtech <synthtech0@gmail.com>
2021-06-01 01:59:10 -0400

    use gfycat api

--- gfycat-redirect.user.js
+++ gfycat-redirect.user.js
@@ -2,24 +2,36 @@
 // @name         Gfycat Redirect
 // @namespace    https://github.com/synthtech
 // @description  Redirect gfycat pages to source video
-// @version      1.0.1
+// @version      2.0.0
 // @author       synthtech
-// @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@ec863aa92cea78a20431f92e80ac0e93262136df/wait-for-elements/wait-for-elements.js
 // @match        *://gfycat.com/*
 // @match        *://www.redgifs.com/*
-// @match        *://www.gifdeliverynetwork.com/*
 // @grant        none
 // ==/UserScript==
 
 (() => {
-    waitForElems({
-        sel: 'video',
-        stop: true,
-        onmatch(player) {
-            if (player) {
-                const vid = Array.from(player.childNodes).find((node) => node.type === 'video/mp4' && !node.src.includes('-mobile'));
-                location.replace(vid.src);
-            }
-        },
+    const REGEX = /^https?:\/\/(www\.)?(gfycat\.com|redgifs\.com)\/(watch\/)?([A-Za-z-]+)$/;
+
+    async function fetchVid(host, id) {
+        const res = await fetch(`https://api.${host}/v1/gfycats/${id}`);
+        if (res.ok) {
+            const { gfyItem } = await res.json();
+            return gfyItem.mp4Url;
+        }
+        return null;
+    }
+
+    async function checkLink(url) {
+        const [,, host,, slug] = url.match(REGEX);
+        const id = slug.split('-')[0];
+        return fetchVid(host, id);
+    }
+
+    waitForUrl(REGEX, async (url) => {
+        const link = await checkLink(url);
+        if (link) {
+            location.replace(link);
+        }
     });
 })();
