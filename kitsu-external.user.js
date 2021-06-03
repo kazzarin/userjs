32974a58dacd722ea34bc7096d02c8632b85a6fa
synthtech <synthtech0@gmail.com>
2021-06-02 23:41:06 -0400

    rewrite kitsu external links

--- kitsu-external.user.js
+++ kitsu-external.user.js
@@ -2,7 +2,7 @@
 // @name         Kitsu External Links
 // @namespace    https://github.com/synthtech
 // @description  Add external links to Kitsu media pages
-// @version      1.0.2
+// @version      2.0.0
 // @author       synthtech
 // @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@ec863aa92cea78a20431f92e80ac0e93262136df/wait-for-elements/wait-for-elements.js
 // @match        *://kitsu.io/*
@@ -12,93 +12,122 @@
 (() => {
     const REGEX = /^https?:\/\/kitsu\.io\/(anime|manga)\/([^/]+)\/?(?:\?.*)?$/;
 
-    const App = {
-        getMappings(slug, media, cb) {
-            fetch('https://kitsu.io/api/graphql', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    query: `query ($slug: String!) { ${this.getMediaQuery(media)}(slug: $slug) { mappings(first: 20) { nodes { externalId externalSite } } } }`,
-                    variables: { slug },
-                }),
-            })
-                .then((res) => res.json())
-                .then(({ data }) => {
-                    let mappings;
-                    if (data) {
-                        if (media === 'anime') mappings = data.findAnimeBySlug.mappings.nodes;
-                        else if (media === 'manga') mappings = data.findMangaBySlug.mappings.nodes;
-                    }
-                    cb(mappings);
-                });
+    const mappingLinks = [
+        {
+            key: 'MYANIMELIST_ANIME',
+            title: 'MyAnimeList',
+            link: 'https://myanimelist.net/anime/',
         },
-        getMediaQuery(media) {
-            if (media === 'anime') {
-                return 'findAnimeBySlug';
-            }
-            return 'findMangaBySlug';
+        {
+            key: 'MYANIMELIST_MANGA',
+            title: 'MyAnimeList',
+            link: 'https://myanimelist.net/manga/',
         },
-    };
+        {
+            key: 'ANILIST_ANIME',
+            title: 'AniList',
+            link: 'https://anilist.co/anime/',
+        },
+        {
+            key: 'ANILIST_MANGA',
+            title: 'AniList',
+            link: 'https://anilist.co/manga/',
+        },
+        {
+            key: 'MANGAUPDATES',
+            title: 'MangaUpdates',
+            link: 'https://www.mangaupdates.com/series.html?id=',
+        },
+    ];
 
-    waitForUrl(REGEX, () => {
-        const [url, media, slug] = location.href.match(REGEX);
+    async function fetchMappings(media, slug) {
+        const mediaQuery = media === 'anime' ? 'findAnimeBySlug' : 'findMangaBySlug';
+        const res = await fetch('https://kitsu.io/api/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                query: `query ($slug: String!) { ${mediaQuery}(slug: $slug) { mappings(first: 20) { nodes { externalId externalSite } } } }`,
+                variables: { slug },
+            }),
+        });
+        if (res.ok) {
+            const { data } = await res.json();
+            if (data) {
+                return data[mediaQuery].mappings.nodes;
+            }
+        }
+        return null;
+    }
+
+    async function newElem(type, props) {
+        const elem = document.createElement(type);
+        if (props) {
+            Object.entries(props).forEach((attr) => {
+                const [k, v] = attr;
+                if (k === 'style') {
+                    Object.entries(v).forEach((prop) => {
+                        const [l, w] = prop;
+                        elem.style[l] = w;
+                    });
+                } else {
+                    elem[k] = v;
+                }
+            });
+        }
+        return elem;
+    }
+
+    async function createLinks(mappings, url) {
+        const check = document.querySelector('#external-links');
+        if (check) check.remove();
+
+        if (location.href === url && mappings?.length) {
+            const section = await newElem('section', {
+                id: 'external-links',
+                className: 'media--information',
+            });
+
+            const header = await newElem('h5', {
+                textContent: 'External Links',
+            });
+
+            const listWrap = await newElem('ul');
+
+            mappings.forEach(async (site) => {
+                const map = mappingLinks.find((extLink) => extLink.key === site.externalSite);
+                if (map) {
+                    const list = await newElem('li');
+                    const link = await newElem('a', {
+                        target: '_blank',
+                        rel: 'noopener noreferrer',
+                        href: map.link + site.externalId,
+                        textContent: map.title,
+                    });
+                    list.appendChild(link);
+                    listWrap.appendChild(list);
+                }
+            });
+
+            section.appendChild(header);
+            section.appendChild(listWrap);
+            return section;
+        }
+        return null;
+    }
+
+    waitForUrl(REGEX, (url) => {
+        const [, media, slug] = url.match(REGEX);
         waitForElems({
             sel: '.media-summary',
             stop: true,
-            onmatch(node) {
-                App.getMappings(slug, media, (mappings) => {
-                    const check = document.querySelector('#external-links');
-                    if (check) check.remove();
-
-                    if (location.href === url && mappings?.length) {
-                        const frag = new DocumentFragment();
-                        const section = document.createElement('section');
-                        section.id = 'external-links';
-                        section.className = 'media--information';
-
-                        const header = document.createElement('h5');
-                        header.textContent = 'External Links';
-                        section.append(header);
-
-                        const listWrap = document.createElement('ul');
-
-                        mappings.forEach((site) => {
-                            const list = document.createElement('li');
-                            const link = document.createElement('a');
-                            link.target = '_blank';
-                            link.rel = 'noopener noreferrer';
-                            switch (site.externalSite) {
-                            case 'MYANIMELIST_ANIME':
-                                link.href = `https://myanimelist.net/anime/${site.externalId}`;
-                                link.textContent = 'MyAnimeList';
-                                break;
-                            case 'MYANIMELIST_MANGA':
-                                link.href = `https://myanimelist.net/manga/${site.externalId}`;
-                                link.textContent = 'MyAnimeList';
-                                break;
-                            case 'ANILIST_ANIME':
-                                link.href = `https://anilist.co/anime/${site.externalId}`;
-                                link.textContent = 'AniList';
-                                break;
-                            case 'ANILIST_MANGA':
-                                link.href = `https://anilist.co/manga/${site.externalId}`;
-                                link.textContent = 'AniList';
-                                break;
-                            case 'MANGAUPDATES':
-                                link.href = `https://www.mangaupdates.com/series.html?id=${site.externalId}`;
-                                link.textContent = 'MangaUpdates';
-                                break;
-                            default:
-                            }
-                            list.append(link);
-                            listWrap.append(list);
-                        });
-
-                        section.append(listWrap);
-                        frag.append(section);
-                        node.append(frag);
+            onmatch: async (node) => {
+                const mappings = await fetchMappings(media, slug);
+                if (mappings) {
+                    const links = await createLinks(mappings, url);
+                    if (links) {
+                        node.append(links);
                     }
-                });
+                }
             },
         });
     });
