c2f217cd3fdc763fe5f2856b7b80bf082612ffb3
synthtech <synthtech0@gmail.com>
2021-06-08 23:11:14 -0400

    Update gfycat-redirect.user.js

--- gfycat-redirect.user.js
+++ gfycat-redirect.user.js
@@ -2,12 +2,13 @@
 // @name         Gfycat Redirect
 // @namespace    https://github.com/synthtech
 // @description  Redirect gfycat pages to source video
-// @version      2.0.1
+// @version      2.0.2
 // @author       synthtech
 // @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@ec863aa92cea78a20431f92e80ac0e93262136df/wait-for-elements/wait-for-elements.js
 // @match        *://gfycat.com/*
 // @match        *://www.redgifs.com/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (() => {
@@ -28,10 +29,12 @@
         return fetchVid(host, id);
     }
 
-    waitForUrl(REGEX, async (url) => {
+    async function redirect(url) {
         const link = await checkLink(url);
         if (link) {
             location.assign(link);
         }
-    });
+    }
+
+    waitForUrl(REGEX, async (url) => redirect(url));
 })();
