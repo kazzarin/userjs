a381d9b921e25500eb8e674b69de27a57b3b08fa
synthtech <synthtech0@gmail.com>
2021-09-15 00:28:12 -0400

    remove waitforelems dep on anilist submission

--- anilist-submission.user.js
+++ anilist-submission.user.js
@@ -2,61 +2,80 @@
 // @name         AniList Submission Links
 // @namespace    https://github.com/synthtech
 // @description  Add links to submissions on user profiles
-// @version      1.1.4
+// @version      1.2.0
 // @author       synthtech
-// @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@ec863aa92cea78a20431f92e80ac0e93262136df/wait-for-elements/wait-for-elements.js
 // @match        *://anilist.co/*
 // @grant        none
 // ==/UserScript==
 
 (() => {
-    const REGEX = /^https?:\/\/anilist\.co\/user\/([A-Za-z0-9]+)(\/.*)?$/;
+    const regex = /\/user\/([A-Za-z0-9]+)(\/.*)?/;
 
     function navigate() {
         const app = document.getElementById('app');
-        app.__vue__._router.push({ name: 'UserSubmissions' });
+        app.__vue__.$router.push({ name: 'UserSubmissions' });
     }
 
     function clickHandler(e) {
-        if (e.target.id === 'submissions-link') {
+        if (e.target?.id === 'submissions-link') {
             e.preventDefault();
             navigate();
         }
     }
 
+    async function addLink(elem) {
+        const currentUser = JSON.parse(localStorage.getItem('auth')).name;
+        const [, user] = location.pathname.match(regex);
+        const checkLink = elem.querySelector('#submissions-link');
+        if (checkLink) {
+            if (user === currentUser) {
+                checkLink.removeEventListener('click', clickHandler);
+                checkLink.remove();
+            } else {
+                checkLink.href = `/user/${user}/submissions`;
+            }
+        } else if (user !== currentUser) {
+            const dataAttr = document.querySelector('.header-wrap').__vue__.$options._scopeId;
+            const link = document.createElement('a');
+            link.href = `/user/${user}/submissions`;
+            link.className = 'link';
+            link.setAttribute(dataAttr, '');
+            link.textContent = 'Submissions';
+            link.id = 'submissions-link';
+            link.addEventListener('click', clickHandler);
+            elem.appendChild(link);
+        }
+    }
+
+    async function watchElem(watch, func) {
+        const mut = new MutationObserver(async (_mutations, observer) => {
+            const elem = document.querySelector(watch);
+            if (elem) {
+                observer.disconnect();
+                await func(elem);
+            }
+        });
+        mut.observe(document.body, { subtree: true, childList: true });
+    }
+
+    async function routeWatch() {
+        const app = document.getElementById('app');
+        if (app.__vue__) {
+            app.__vue__.$watch('$route', async (newRoute) => {
+                if (regex.test(newRoute.path)) {
+                    await watchElem('.user .nav.container', addLink);
+                }
+            });
+        } else {
+            setTimeout(routeWatch, 100);
+        }
+    }
+
     // Not accessible unless logged in
     if (localStorage.getItem('auth')) {
-        const currentUser = JSON.parse(localStorage.getItem('auth')).name;
-
-        waitForUrl(REGEX, () => {
-            const [, user] = location.href.match(REGEX);
-            waitForElems({
-                sel: '.header-wrap .nav-wrap .nav.container',
-                onmatch(elem) {
-                    const checkLink = elem.querySelector('#submissions-link');
-
-                    if (checkLink) {
-                        if (user === currentUser) {
-                            checkLink.removeEventListener('click', clickHandler);
-                            checkLink.remove();
-                        } else {
-                            checkLink.href = `/user/${user}/submissions`;
-                        }
-                    } else if (user !== currentUser) {
-                        const dataAttr = document.querySelector('.header-wrap').__vue__.$options._scopeId;
-
-                        const link = document.createElement('a');
-                        link.href = `/user/${user}/submissions`;
-                        link.className = 'link';
-                        link.setAttribute(dataAttr, '');
-                        link.textContent = 'Submissions';
-                        link.id = 'submissions-link';
-
-                        link.addEventListener('click', clickHandler);
-                        elem.appendChild(link);
-                    }
-                },
-            });
-        });
+        if (regex.test(location.pathname)) {
+            watchElem('.user .nav.container', addLink);
+        }
+        routeWatch();
     }
 })();
