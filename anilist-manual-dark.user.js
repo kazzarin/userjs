b792e7918e7a4acfcd6485b9da8d1bd9307c4b59
synthtech <synthtech0@gmail.com>
2021-12-08 20:38:38 -0500

    Create anilist-manual-dark.user.js

---
+++ anilist-manual-dark.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         AniList Submission Manual Dark Mode Fix
+// @namespace    https://github.com/synthtech
+// @description  Toggles dark mode automatically
+// @version      1.0.0
+// @author       synthtech
+// @match        https://submission-manual.anilist.co/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    function toggle(elem) {
+        elem.parentNode.click();
+    }
+
+    function watchElem(watch, func) {
+        new MutationObserver((_mutations, observer) => {
+            const elem = document.querySelector(watch);
+            if (elem) {
+                func(elem);
+                observer.disconnect();
+            }
+        }).observe(document.body, { subtree: true, childList: true });
+    }
+
+    watchElem('.toggle-mode > div[title="Change to Dark Mode"]', toggle);
+})();
