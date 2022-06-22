c9f24cf8fdbb6183446a9dccdcd9aa2d347c1baa
Zarin <kazzarin@protonmail.com>
2022-06-22 19:50:52 -0400

    switch to vm-dom and use localstorage for settings

--- kitsu-library-link.user.js
+++ kitsu-library-link.user.js
@@ -2,34 +2,60 @@
 // @name         Kitsu Library Link
 // @namespace    https://github.com
 // @description  Forces library parameters in the header link
-// @version      1.0.3
+// @version      2.0.0
 // @license      0BSD
 // @author       Zarin
-// @require      https://cdn.jsdelivr.net/gh/fuzetsu/userscripts@b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
-// @match        *://kitsu.io/*
+// @require      https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @match        https://kitsu.io/*
 // @grant        none
 // ==/UserScript==
 
+/* global VM */
+
 (() => {
-    const MEDIA = 'manga';
-    const STATUS = 'current';
-    const REGEX = /\/users\/([a-zA-Z0-9_]+)\/library$/;
+    let settings;
 
-    const App = {
-        openLink(link) {
-            const newLink = `/users/${link.match(REGEX)[1]}/library?media=${MEDIA}&status=${STATUS}`;
-            location.href = newLink;
-        },
-    };
+    function clickHandler() {
+        const newLink = new URL(`${location.origin}/users/${settings.user}/library`);
+        if (settings.type !== 'anime') {
+            newLink.searchParams.set('media', settings.type);
+        }
+        if (settings.status !== 'all') {
+            newLink.searchParams.set('status', settings.status);
+        }
+        location.assign(newLink);
+    }
 
-    waitForElems({
-        sel: '#kitsu-navbar #exCollapsingNavbar2 li:first-child a',
-        stop: true,
-        onmatch(elem) {
-            const link = elem.href;
-            if (link.match(REGEX)) {
-                elem.addEventListener('click', () => { App.openLink(link); }, { once: true, passive: true });
+    function init(nav) {
+        const [, user] = nav.href.match(/\/users\/(\w+)/);
+        settings = JSON.parse(localStorage.getItem('library-link'));
+        if (!settings) {
+            settings = { type: 'anime', status: 'all', user };
+            localStorage.setItem('library-link', JSON.stringify(settings));
+        } else {
+            localStorage.setItem('library-link', JSON.stringify(Object.assign(settings, { user })));
+        }
+        VM.observe(document.body, () => {
+            const node = document.querySelector('#kitsu-navbar #exCollapsingNavbar2 li:first-child a'); // library link
+            if (node) {
+                node.addEventListener('click', clickHandler);
+                return true;
             }
-        },
-    });
+            return false;
+        });
+    }
+
+    const userNav = document.querySelector('#kitsu-navbar .user-menu-drop a:first-child'); // profile link
+    if (userNav) {
+        init(userNav);
+    } else {
+        VM.observe(document.body, () => {
+            const node = document.querySelector('#kitsu-navbar .user-menu-drop a:first-child');
+            if (node) {
+                init(node);
+                return true;
+            }
+            return false;
+        });
+    }
 })();
